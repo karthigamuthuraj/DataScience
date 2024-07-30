b16c4208940090c9ecc83a4d29023620f82795f9
Karthiga Aaru <karthiga.aaru09@gmail.com>
2024-07-30 18:04:55 +0530

    IQR

--- Univariate Analysis/UnivariateAnalyser.py
+++ Univariate Analysis/UnivariateAnalyser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class UnivariateAnalyser:
     def __init__(self, file_path):
@@ -10,22 +11,30 @@ class UnivariateAnalyser:
         # Using select_dtypes to select columns by data types
         self.quantitative = self.dataset.select_dtypes(include=['number']).columns.tolist()
         self.qualitative = self.dataset.select_dtypes(exclude=['number']).columns.tolist()
-        
         return self.quantitative, self.qualitative
-    
-    
+
     def calculate_statistics(self):
         # Ensure quantitative columns are categorized
         if not self.quantitative:
             self.categorize_columns()
 
-        # Create lists to hold statistics
-        descriptive = pd.DataFrame(index=["Mean","Median","Mode"],columns=self.qualitative)
+        # Create a DataFrame to hold statistics
+        descriptive = pd.DataFrame(index=["Mean", "Median", "Mode", "Q1:25%", "Q2:50%", "Q3:75%", "Q4:100%","IQR","1.5Rule","LesserOutlier","GreaterOutlier","Min","Max"], columns=self.quantitative)
 
         # Calculate statistics for each quantitative column
         for col in self.quantitative:
-            descriptive[col]["Mean"] = self.dataset[col].mean()
-            descriptive[col]["Median"] = self.dataset[col].median()
-            descriptive[col]["Mode"] = self.dataset[col].mode()[0]  # mode() returns a Series, get the first mode value
+            descriptive.at["Mean", col] = self.dataset[col].mean()
+            descriptive.at["Median", col] = self.dataset[col].median()
+            descriptive.at["Mode", col] = self.dataset[col].mode().iloc[0] if not self.dataset[col].mode().empty else np.nan
+            descriptive.at["Q1:25%", col] = np.percentile(self.dataset[col].dropna(), 25)
+            descriptive.at["Q2:50%", col] = np.percentile(self.dataset[col].dropna(), 50)
+            descriptive.at["Q3:75%", col] = np.percentile(self.dataset[col].dropna(), 75)
+            descriptive.at["Q4:100%", col] = np.percentile(self.dataset[col].dropna(), 100)
+            descriptive.at["IQR", col] = descriptive.at["Q3:75%", col] - descriptive.at["Q1:25%", col]
+            descriptive.at["1.5Rule", col] = 1.5*descriptive.at["IQR", col]
+            descriptive.at["LesserOutlier", col] = descriptive.at["Q1:25%", col]-descriptive.at["1.5Rule", col]
+            descriptive.at["GreaterOutlier", col] = descriptive.at["Q3:75%", col]+descriptive.at["1.5Rule", col]
+            descriptive.at["Min", col] = self.dataset[col].min()
+            descriptive.at["Max", col] = self.dataset[col].max()
         
         return descriptive
